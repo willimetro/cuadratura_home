ef80238239feeb02f06b41887d95089f52c7814f
SOPORTEONLINE <entel.Online.soporteN2@everis.com>
2019-06-07 16:53:05 -0400

    Cambios generar directorio logs eliminacionn

--- CuadraturaHome/src/cl/everis/cuadratura/util/LogEliminacion.java
+++ CuadraturaHome/src/cl/everis/cuadratura/util/LogEliminacion.java
@@ -1,5 +1,6 @@
 package cl.everis.cuadratura.util;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,15 +11,17 @@ public class LogEliminacion {
 
 	private static FileWriter fw = null;
 	private static PrintWriter pw = null;
-	private final static String FILE_LOG = "C:\\Users\\Administrador\\"
-			+ "Desktop\\Documentos\\EntelProyectoFijo\\cuadratura\\"
-			+ "LogEliminacion\\LogEliminacion_";
+	private final static String FILE_LOG = System.getProperty("user.home")+"\\Desktop\\cuadratura\\LogEliminacion\\LogEliminacion_";
+	private final static File DIR_LOG=new File(System.getProperty("user.home")+"\\Desktop\\cuadratura\\LogEliminacion");
 
 	public static void iniciarFicheros(){
 		try
 		{
+			if (!DIR_LOG.exists()){
+				DIR_LOG.mkdirs();
+			}
 			if (null == fw){
-				fw = new FileWriter(FILE_LOG+LocalDateTime.now().format(DateTimeFormatter.ofPattern("ddMMyyyy"))+".csv" , true);
+				fw = new FileWriter(FILE_LOG+LocalDateTime.now().format(DateTimeFormatter.ofPattern("ddMMyyyy.ss"))+".csv" , true);
 				pw = new PrintWriter(fw);
 			}
 		} catch (Exception e) {
