46f9977b237813b937b559f99e15e350917d65be
Jhonatan Arenas <jhonatan.arenas.salazar@gmail.com>
2020-02-17 17:54:26 -0300

    Se actualiza dump, se agregan querys de splunk

--- DBScript.sql
+++ DBScript.sql
@@ -5,31 +5,11 @@
 -- Dumped from database version 12.1
 -- Dumped by pg_dump version 12.1
 
--- Started on 2019-12-11 15:26:52
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-DROP DATABASE cuadratura_hogar;
---
--- TOC entry 2952 (class 1262 OID 16394)
--- Name: cuadratura_hogar; Type: DATABASE; Schema: -; Owner: admin_hogar
---
-
+-- Started on 2020-02-17 17:49:25
 CREATE DATABASE cuadratura_hogar WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'Spanish_Chile.1252' LC_CTYPE = 'Spanish_Chile.1252';
 
 
 ALTER DATABASE cuadratura_hogar OWNER TO admin_hogar;
-
-\connect cuadratura_hogar
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -107,7 +87,7 @@ ALTER TABLE public.canales_3play_red OWNER TO admin_hogar;
 
 CREATE TABLE public.canales_kaltura (
     "KEY_CANAL" character varying(30),
-    "DOMAIN_EXTERNAL_ID" character varying(15),
+    "DOMAIN_EXTERNAL_ID" character varying(50),
     "MODULE_ID" character varying(10),
     "MODULE_NAME" character varying(30),
     "PLAN_DEPORTES" character varying(20),
@@ -216,6 +196,31 @@ CREATE TABLE public.facturador_kenan_canal (
 
 
 ALTER TABLE public.facturador_kenan_canal OWNER TO admin_hogar;
+
+--
+-- TOC entry 230 (class 1259 OID 16509)
+-- Name: int_3play_no_kenan_idfact; Type: TABLE; Schema: public; Owner: admin_hogar
+--
+
+CREATE TABLE public.int_3play_no_kenan_idfact (
+    "RUT" character varying(20),
+    "RUT_SIN_DV" character varying(20),
+    "CUENTA_SAPRMCA" character varying(20),
+    "ID_FACT_KENAN" character varying(50),
+    "ESTADO_FAC" character varying(5),
+    "DESC_ESTADO_FACT" character varying(50),
+    "FECHA_ESTADO_OXT" character varying(20),
+    "FECHA_ENVIO_XML" character varying(20),
+    "TIPO_TERMINO" character varying(25),
+    "CODIGO_SERVICIO" character varying(25),
+    "NRO_COMERCIAL" character varying(20),
+    "ESTADO_OXC" character varying(5),
+    "ID_BANDA_ANCHA" character varying(50),
+    "ID_CUENTA_TV" character varying(30)
+);
+
+
+ALTER TABLE public.int_3play_no_kenan_idfact OWNER TO admin_hogar;
 
 --
 -- TOC entry 211 (class 1259 OID 16422)
@@ -558,231 +563,7 @@ CREATE TABLE public.voz_bscs (
 
 ALTER TABLE public.voz_bscs OWNER TO admin_hogar;
 
---
--- TOC entry 2919 (class 0 OID 16395)
--- Dependencies: 202
--- Data for Name: bdservicios_retirados; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2920 (class 0 OID 16398)
--- Dependencies: 203
--- Data for Name: canales_3play_62; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2943 (class 0 OID 16467)
--- Dependencies: 226
--- Data for Name: canales_3play_red; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2921 (class 0 OID 16401)
--- Dependencies: 204
--- Data for Name: canales_kaltura; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2922 (class 0 OID 16404)
--- Dependencies: 205
--- Data for Name: cod_kal_tplay; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2923 (class 0 OID 16407)
--- Dependencies: 206
--- Data for Name: cod_serv_kaltura; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2924 (class 0 OID 16410)
--- Dependencies: 207
--- Data for Name: cod_serv_tplay; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2925 (class 0 OID 16413)
--- Dependencies: 208
--- Data for Name: facturador_kenan; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2926 (class 0 OID 16416)
--- Dependencies: 209
--- Data for Name: facturador_kenan_62; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2927 (class 0 OID 16419)
--- Dependencies: 210
--- Data for Name: facturador_kenan_canal; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2928 (class 0 OID 16422)
--- Dependencies: 211
--- Data for Name: internet_3play; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2929 (class 0 OID 16425)
--- Dependencies: 212
--- Data for Name: internet_3play_62; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2945 (class 0 OID 16474)
--- Dependencies: 228
--- Data for Name: internet_3play_red; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2930 (class 0 OID 16428)
--- Dependencies: 213
--- Data for Name: internet_aaa; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2931 (class 0 OID 16431)
--- Dependencies: 214
--- Data for Name: internet_bscs; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2932 (class 0 OID 16434)
--- Dependencies: 215
--- Data for Name: tlf_3play; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2933 (class 0 OID 16437)
--- Dependencies: 216
--- Data for Name: tlf_3play_62; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2946 (class 0 OID 16477)
--- Dependencies: 229
--- Data for Name: tlf_3play_red; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2934 (class 0 OID 16440)
--- Dependencies: 217
--- Data for Name: tlf_otcar; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2935 (class 0 OID 16443)
--- Dependencies: 218
--- Data for Name: todo_kaltura; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2936 (class 0 OID 16446)
--- Dependencies: 219
--- Data for Name: tv_3play_62; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2944 (class 0 OID 16470)
--- Dependencies: 227
--- Data for Name: tv_3play_red; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2937 (class 0 OID 16449)
--- Dependencies: 220
--- Data for Name: tv_bscs; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2938 (class 0 OID 16452)
--- Dependencies: 221
--- Data for Name: tv_intraway; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2939 (class 0 OID 16455)
--- Dependencies: 222
--- Data for Name: tv_kaltura; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2940 (class 0 OID 16458)
--- Dependencies: 223
--- Data for Name: tv_vmica_bscs; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2941 (class 0 OID 16461)
--- Dependencies: 224
--- Data for Name: tvcanales_3play; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
---
--- TOC entry 2942 (class 0 OID 16464)
--- Dependencies: 225
--- Data for Name: voz_bscs; Type: TABLE DATA; Schema: public; Owner: admin_hogar
---
-
-
-
--- Completed on 2019-12-11 15:26:52
+-- Completed on 2020-02-17 17:49:26
 
 --
 -- PostgreSQL database dump complete
